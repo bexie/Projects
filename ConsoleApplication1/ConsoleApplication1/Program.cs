37af3e96c68553f83a3fe3952d3c44aa8f583a99
bella <bella.xie.81@gmail.com>
2014-01-28 15:00:42 +1100

    still ConsoleApplication1 had not been built, test again

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            System.Console.Write("Hello MSBuild2");
+            System.Console.Write("Hello MSBuild22");
         }
     }
 }
